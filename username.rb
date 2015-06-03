6351d690c78e648ad5fc7e6135a6cc761d8af9f3
Ugo Aniukwu <ugo@smallhaxe.com>
2015-06-02 23:26:08 -0700

    8/14

--- username.rb
+++ username.rb
@@ -8,3 +8,15 @@ def generate_username2(firstName, lastName)
   (firstName != '' && lastName != '') ?
     (firstName.gsub(/[^A-Za-z]/, '').strip[0] + lastName.gsub(/[^A-Za-z]/, '').strip).downcase : nil
 end
+
+def generate_username3(firstName, lastName, birthYear)
+  conditions = ((firstName != '' && lastName != '' && birthYear != '') && (birthYear.integer? && birthYear.to_s.length == 4 && birthYear >= 1900 && birthYear <= Time.now.year))
+  if (conditions)
+    finit = firstName.gsub(/[^A-Za-z]/, '').strip[0]
+    lname = lastName.gsub(/[^A-Za-z]/, '').strip
+    byear = birthYear.to_s.strip[-2,2]
+    finit + lname + byear
+  else
+    nil
+  end
+end
