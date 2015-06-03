14dd9dee50de0fcf4c500168fff4090a3f741590
Ugo Aniukwu <ugo@smallhaxe.com>
2015-06-02 22:18:04 -0700

    2/14

--- username.rb
+++ username.rb
@@ -3,3 +3,8 @@
 def generate_username1(firstName)
   firstName[0].downcase
 end
+
+def generate_username2(firstName, lastName)
+  (firstName != '' && lastName != '') ?
+    (firstName.gsub(/[^A-Za-z]/, '').strip[0] + lastName.gsub(/[^A-Za-z]/, '').strip).downcase : nil
+end
